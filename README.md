d155e8fe357d071dfd0e8d6700c075c37e56fca8
Fernanda Aro <fernanda_aro@yahoo.com>
2023-10-04 10:58:40 -0300

    adicionando pasta vscode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # segredo
+ 
  Código criado durante as aulas de Graduação no Senac entre Agosto e Setembro/2023
---
+++ vscode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace segredo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random randNum = new Random();
+            int sorteio = randNum.Next(10);
+
+            int usuario;
+            int tentativas = 5;
+
+            Console.WriteLine("Você tem 5 tentativas para adivinhar o número secreto...");
+
+            while (tentativas > 0)
+            {
+                Console.WriteLine("Digite um número: ");
+                usuario = int.Parse(Console.ReadLine());
+
+                if (usuario == sorteio)
+                {
+                    Console.WriteLine("Parabéns, você acertou!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opss... Você errou...");
+                }
+                tentativas = tentativas - 1;
+            }
+            Console.WriteLine("O número secreto era... " + sorteio);
+        }
+    }
+}
